fc0bd34b3b9b6e2a2f43c29543b3aa986e802881
cedricbriand <briandcedric@sfr.fr>
2017-03-08 14:56:26 +0100

    commit

---
+++ src/app/Animaux/animal.ts
@@ -0,0 +1,9 @@
+export class Animal {
+  id : number;
+  type:string;
+  nom:string;
+  race : string;
+  sexe:string;
+  note:Array<number>;
+  date_de_naissance:string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Animal } from './animal';
+import { ANIMAUX } from './mock-animaux';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Comme chiens et chats !!!';
+  animaux:Animal[]=null;
+
+  ngOnInit(){
+    this.animaux=ANIMAUX;
+  }
+  selectAnimal(animal:Animal) {
+    console.log('vous avez selectionné '+ animal.nom);
+  }
 }
