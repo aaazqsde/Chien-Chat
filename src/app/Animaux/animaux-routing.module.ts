94c161fb31effb48adfec3419467eb4da0d03487
Hanison <lenoir.guillaume@yahoo.fr>
2017-03-09 14:52:39 +0100

    Ajout du module AnimauxModule

---
+++ src/app/Animaux/animaux-routing.module.ts
@@ -0,0 +1,27 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core'
+
+import {TempoComponent} from './tempo.component';
+import {ListAnimalComponent} from './list-animal/list-animal.component';
+import {ListChienComponent} from './list-animal/list-chien.component';
+import {ListChatComponent} from './list-animal/list-chat.component';
+import {DetailAnimalComponent} from './detail-animal.component';
+
+
+
+const animauxRoutes: Routes = [
+  { path: 'animaux', component: ListAnimalComponent},
+  { path: 'animaux/:id', component: DetailAnimalComponent},
+  { path: 'chiens', component: ListChienComponent},
+  { path: 'chiens/:id', component: DetailAnimalComponent},
+  { path: 'chats', component: ListChatComponent},
+  { path: 'chats/:id', component: DetailAnimalComponent}
+
+];
+
+@NgModule ({
+  imports: [RouterModule.forChild(animauxRoutes)],
+  exports: [RouterModule]
+})
+
+export class AnimauxRoutingModule {}
---
+++ src/app/Animaux/animaux.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {ListAnimalComponent} from './list-animal/list-animal.component';
+import {ListChienComponent} from './list-animal/list-chien.component';
+import {ListChatComponent} from './list-animal/list-chat.component';
+import {DetailAnimalComponent} from './detail-animal.component';
+import {TempoComponent} from './tempo.component';
+
+import {AnimauxRoutingModule} from './animaux-routing.module';
+
+@NgModule({
+  imports: [CommonModule, AnimauxRoutingModule],
+  declarations: [
+    ListAnimalComponent,
+    ListChienComponent,
+    ListChatComponent,
+    DetailAnimalComponent,
+    TempoComponent
+  ],
+  providers: []
+})
+export class AnimauxModule {}
