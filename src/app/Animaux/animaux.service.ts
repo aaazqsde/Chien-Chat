0c1524e12751ed2f84365006aef0f7692c49e734
Hanison <lenoir.guillaume@yahoo.fr>
2017-03-09 16:34:16 +0100

    mise a jour du service

--- src/app/Animaux/animaux.service.ts
+++ src/app/Animaux/animaux.service.ts
@@ -9,6 +9,46 @@ export class AnimauxService {
 getAnimaux():Animal[]{
   return ANIMAUX;
 }
+
+getChiens(): Animal[]{
+  let chiens: Animal[]=[];
+  for (let ani of ANIMAUX){
+    if (ani.type === "chien"){
+      chiens.push(ani);
+    }
+  }
+  return chiens;
+}
+
+getChats(): Animal[]{
+  let chats: Animal[]=[];
+  for (let ani of ANIMAUX){
+    if (ani.type === "chat"){
+      chats.push(ani);
+    }
+  }
+  return chats;
+}
+
+getChat(id: number){
+  let chats = this.getChats();
+  for(let index=0;index<chats.length;index++){
+    if(id==chats[index].id){
+      return chats[index];
+    }
+  }
+}
+
+getChien(id: number){
+  let chiens = this.getChiens();
+  for(let index=0;index<chiens.length;index++){
+    if(id==chiens[index].id){
+      return chiens[index];
+    }
+  }
+}
+
+
 getAnimal(id:number){
   let animaux = this.getAnimaux();
 
@@ -18,5 +58,6 @@ getAnimal(id:number){
     }
   }
 }
+
 
 }
--- src/app/Animaux/list-animal/list-chat.component.ts
+++ src/app/Animaux/list-animal/list-chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Animal }from 'app/Animaux/animal';
 import { ANIMAUX } from 'app/Animaux/mock-animaux';
 import { Router } from '@angular/router';
+import { AnimauxService } from 'app/Animaux/animaux.service';
 
 @Component({
   selector: 'app-list-chat',
@@ -13,19 +14,17 @@ export class ListChatComponent implements OnInit {
   chats : Animal[]=null;
 
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private animauxService : AnimauxService) { }
 
   ngOnInit():void {
-    this.animaux=ANIMAUX;
-    this.chats=[];
-
-      for (let ani of this.animaux){
-        if (ani.type==="chat"){
-          this.chats.push(ani);
-        }
-      }
+    this.getChats();
 
   }
+
+  getChats(){
+    this.chats = this.animauxService.getChats();
+  }
+
   selectAnimal(animal:Animal):void{
     console.log('Vous avez selectionné '+ animal.nom);
     let link = ['chats', animal.id];
